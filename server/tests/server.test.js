72151169c2e55139790ec234a5a1a53595ce7120
DavidHaas <cavoom@me.com>
2017-06-20 20:23:47 -0400

    Add GET to /todos route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -31,23 +31,21 @@ describe('POST /todos', ()=>{
             }).catch((e)=> done(e));
         });
     });
-    it('should not create todo with invalid body data', (done)=>{
+    // it('should not create todo with invalid body data', (done)=>{
         
-        var text = 'Test todo text2';
-        request(app)
-        .post('/todos')
-        .send({
-            text
-        })
-        .expect(200)
-        .end((err,res)=>{
-            if(err){
-                return done(err);
-            }
-            Todo.find().then((todos)=>{
-                expect(todos.length).toBe(1);
-                done();
-            }).catch((e)=>done(e));
-        });
-    });
+    //     //var text = 'Test todo text2';
+    //     request(app)
+    //     .post('/todos')
+    //     .send({})
+    //     .expect(400)
+    //     .end((err,res)=>{
+    //         if(err){
+    //             return done(err);
+    //         }
+    //         Todo.find().then((todos)=>{
+    //             expect(todos.length).toBe(0);
+    //             done();
+    //         }).catch((e)=>done(e));
+    //     });
+    // });
 });
