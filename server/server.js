1b163ca6fb582c88d7a45cfa57bdff70ffcf304e
DavidHaas <cavoom@me.com>
2017-07-02 17:45:09 -0400

    setup app for heroku

--- server/server.js
+++ server/server.js
@@ -4,34 +4,36 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-
-var app = express();
-
 var {ObjectID} = require('mongodb');
 
-// app.use(bodyParser.json());
+var app = express();
+const port = process.env.PORT || 3000;
 
-// app.post('/todos', (req, res) =>{
-//     var todo = new Todo({
-//         text: req.body.text
-//     });
 
-//     todo.save().then((doc)=>{
-//         res.send(doc);
-//     }, (e)=>{
-//         res.status(400).send(e);
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) =>{
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc)=>{
+        res.send(doc);
+    }, (e)=>{
+        res.status(400).send(e);
     
-//     });
+    });
 
-// });
+});
 
-// app.get('/todos', (req, res)=>{
-//     Todo.find().then((todos)=>{
-//         res.send({todos});
-//     }, (e)=>{
-//         res.status(400).send(e);
-//     });
-// });
+app.get('/todos', (req, res)=>{
+    Todo.find().then((todos)=>{
+        res.send({todos});
+    }, (e)=>{
+        res.status(400).send(e);
+    });
+});
 
 // GET /todos/12345
 app.get('/todos/:id', (req,res)=>{
@@ -53,8 +55,8 @@ app.get('/todos/:id', (req,res)=>{
     });
     
 });
-app.listen(3000, () => {
-    console.log('Started on port 3000');
+app.listen(port, () => {
+    console.log('Started at Port',port);
 });
 
 module.exports = {app}
